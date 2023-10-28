facc91c5f35b167b04bbfd873aaaecb2784b4bc5
Vincent Rachor <vincent-rachor@web.de>
2023-10-28 22:17:57 +0200

    Improved visuals and implemented method to include Optimal Recognition Point marked in red

--- README.md
+++ README.md
@@ -2,20 +2,22 @@
 
 Designed to enhance reading efficiency and comprehension by displaying text one word at a time at a user-defined pace. This tool aids in improving reading speed and focus, making it particularly useful for those seeking to boost their reading skills. By eliminating the need for traditional eye movements, this tool allows readers to process content more rapidly and is a valuable aid for anyone looking to become a more efficient and effective reader.
 
+Coloring the a letter of the displayed word in red is a way to guide the eye during *Rapid Serial Visual Presentation* (RSVP). In this case, identifying the middle letter of the word and using it as *Optimal Recognition Point* (RSP) is a simple but effective method of RSVP.
+
 ## Install
-```python 
+```python
 pip install PyPDF2 --upgrade
 ```
 
 
-```python 
+```python
 pip install python-docx --upgrade
 ```
 
 
 ## Import Libraries
 
-```python 
+```python
 import tkinter as tk
 from tkinter import filedialog
 from PyPDF2 import PdfReader
@@ -25,3 +27,6 @@ import os
 
 # Sprintre In Action
 ![sprintreshot](https://github.com/UKVeteran/Sprintre/assets/39216339/19d9ea7d-625e-45ed-8436-119cb1460e7e)
+
+
+![Displayed Word](SpeedReaderInAction.png)
--- sprintre.py
+++ sprintre.py
@@ -1,42 +1,45 @@
 import tkinter as tk
 from tkinter import filedialog
 from PyPDF2 import PdfReader
-from docx import Document  
+from docx import Document
 import os
 
 class SprintReader:
     def __init__(self, root):
         self.root = root
         self.root.title("Sprint Reader")
-        
-        self.text = tk.Text(self.root, wrap=tk.WORD, height=10, width=40)
+
+        self.text = tk.Text(self.root, wrap=tk.WORD, height=10, width=40, bg='black', fg='white', font=("TkDefaultFont", 32))
         self.text.pack(pady=10, padx=10)
-        
-        self.start_stop_button = tk.Button(self.root, text="Start", command=self.toggle_start_stop)
-        self.start_stop_button.pack(side=tk.LEFT, padx=5)
-        
-        self.speed_label = tk.Label(self.root, text="Speed (words per minute):")
+
+        self.text.tag_configure('center', justify='center')
+        self.text.tag_configure('middle_letter', foreground='red')
+
+        self.start_stop_button = tk.Button(self.root, text="Start", command=self.toggle_start_stop, width=20, height=2, font=("Arial", 20))
+        self.start_stop_button.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.speed_label = tk.Label(self.root, text="Speed (words per minute):", font=("Arial", 20))
         self.speed_label.pack()
-        
+
         self.speed_slider = tk.Scale(self.root, from_=100, to=1000, orient=tk.HORIZONTAL)
-        self.speed_slider.set(300)  # Initial speed
+        self.speed_slider.set(400)  # Initial speed
         self.speed_slider.pack()
 
-        self.upload_button = tk.Button(self.root, text="Upload File", command=self.upload_file)
-        self.upload_button.pack()
+        self.upload_button = tk.Button(self.root, text="Upload File", command=self.upload_file, width=20, height=2, font=("Arial", 20))
+        self.upload_button.pack(side=tk.RIGHT, padx=5, pady=5)
 
         self.file_path = None
         self.reading = False
         self.words = []
         self.index = 0
         self.after_id = None
-        
+
     def upload_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf"), ("Text files", "*.txt"), ("Word files", "*.docx")])
         if file_path:
             self.file_path = file_path
             self.text.delete("1.0", tk.END)
-            
+
             # Determine the file type and read accordingly
             if file_path.endswith(".pdf"):
                 pdf_reader = PdfReader(file_path)
@@ -50,7 +53,7 @@ class SprintReader:
                     text_content = file.read()
                     self.text.insert(tk.END, text_content)
                     self.words = text_content.split()
-                
+
     def toggle_start_stop(self):
         if not self.reading:
             self.reading = True
@@ -64,9 +67,23 @@ class SprintReader:
 
     def read_word(self):
         if self.reading and self.index < len(self.words):
+            newlines = "\n" * 5
             word = self.words[self.index]
             self.text.delete("1.0", tk.END)
-            self.text.insert(tk.END, word)
+            self.text.insert(tk.END, newlines + word)
+            self.text.tag_add('center', '1.0', 'end')
+
+            #determine word length to place red letter (needs "red_letter" and a "stop_point", which is the letter right after)
+            #behaviour differs depending on odd or even number of letters in word
+            word_len = len(self.words[self.index])
+            if word_len % 2 == 0:
+                red_letter = "6." + str((word_len // 2) - 1)
+                stop_point = "6." + str((word_len // 2) + 0)
+            else:
+                red_letter = "6." + str((word_len // 2) - 0)
+                stop_point = "6." + str((word_len // 2) + 1)
+            self.text.tag_add('middle_letter', red_letter, stop_point)
+
             self.index += 1
             self.after_id = self.root.after(int(60000 / self.speed_slider.get()), self.read_word)
         else:
